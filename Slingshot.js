d059520a38919ac5c7eec99572848275477735ba
Scyphiform <76980214+Scyphiform@users.noreply.github.com>
2021-02-01 20:30:55 -0500

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,48 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+
+        this.img1 = loadImage('sprites/sling1.png');
+        this.img2 = loadImage('sprites/sling2.png');
+        this.img3 = loadImage('sprites/sling3.png');
+
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+
+        image(this.img1,200,50)
+        image(this.img2,170,50)
+
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            stroke(48,27,8)
+
+            if(pointA.x < 220){
+                strokeWeight(4);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+                image(this.img3,pointA.x-30,pointA.y-10,15,30)
+            }else{
+                strokeWeight(4);
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+                image(this.img3,pointA.x-30,pointA.y-10,15,30)
+            }
+            
+        }
+    }
+    
+}
